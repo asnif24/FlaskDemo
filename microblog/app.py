a95729a4a3848a080ddcb9f9e6c85560770a5e9f
HT Lin <asnif24@gmail.com>
2022-12-14 19:58:56 +0800

    datetime

--- microblog/app.py
+++ microblog/app.py
@@ -3,9 +3,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+entries = []
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
-        print(entry_content, datetime.datetime.today())
-    return render_template("home.html")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content, formatted_date))
+
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in entries
+    ]
+
+    return render_template("home.html", entries = entries_with_date)
