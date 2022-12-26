f6a2cc71262db99d2071cb6559ce45f68dee7a36
HT Lin <asnif24@gmail.com>
2022-12-26 18:45:54 +0800

    insert mongo db

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # FlaskDemo
+
+### FLASK Environment Settings
+```
+# for power shell
+$env:FLASK = "app.py"
+$env:FLASK_ENV = "development"
+```
--- microblog/app.py
+++ microblog/app.py
@@ -1,16 +1,21 @@
 import datetime
 from flask import Flask, render_template, request
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
-
+client = MongoClient("mongodb+srv://asnif:asnif5566@microblog-application.2p251u2.mongodb.net/test")
+app.db = client.microblog
 entries = []
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    # print([e for e in app.db.entries.find({})])
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
         entries.append((entry_content, formatted_date))
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
     entries_with_date = [
         (
